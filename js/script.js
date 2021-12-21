e3b82d6392af822b3ef1f7b672fd2705be06643c
niculas1989 <nicolas.maranzano@libero.it>
2021-12-21 14:15:40 +0100

    added analyses in JS

--- js/script.js
+++ js/script.js
@@ -9,5 +9,15 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+Passiamo all'analisi della traccia:
+1 - Chiedere all'utente quanti KM vuole percorrere
+2 - Chiedere all'utente l'età
+3 - calcolare il prezzo del biglietto
+3b - farlo attraverso queste regole:
+    - il costo è di 0.21€ al KM
+    - va applicato uno sconto del 20% per i minorenni (età < 18)
+    - va applicato uno sconto del 40% per gli anziani (età > 65)
 */
 
